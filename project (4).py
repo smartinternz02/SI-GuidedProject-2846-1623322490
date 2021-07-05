eafc6571463902194c4edb6fd7960aa9b1138ff8
sharmila-D4 <84568946+sharmila-D4@users.noreply.github.com>
2021-07-05 17:31:33 +0530

    Add files via upload

---
+++ project (4).py
@@ -0,0 +1,165 @@
+import face_recognition
+import cv2
+import datetime
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
+import sys
+import time
+
+
+# This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
+# other example, but it includes some basic performance tweaks to make things run a lot faster:
+#   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
+#   2. Only detect faces in every other frame of video.
+
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+
+# Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
+
+# Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file(r"G:/Robert.jfif")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Load a second sample picture and learn how to recognize it.
+biden_image = face_recognition.load_image_file(r"G:/PierceBrosnan.jpg")
+biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+
+'''biden_image1 = face_recognition.load_image_file("nikhil.jpg")
+biden_face_encoding1 = face_recognition.face_encodings(biden_image1)[0]'''
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    obama_face_encoding,
+    biden_face_encoding,
+    #biden_face_encoding1
+]
+known_face_names = [
+    "Robert DowneyJR",
+    "Pierce Brosnan",
+    #"Nikhil"
+]
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True:
+    # Grab a single frame of video
+    ret, frame = video_capture.read()
+
+    # Resize frame of video to 1/4 size for faster face recognition processing
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
+    # Only process every other frame of video to save time
+    if process_this_frame:
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            # If a match was found in known_face_encodings, just use the first one.
+            if True in matches:
+                first_match_index = matches.index(True)
+                name = known_face_names[first_match_index]
+
+            face_names.append(name)
+
+    process_this_frame = not process_this_frame
+
+
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+    # Display the resulting image
+    cv2.imshow('Video', frame)
+    cv2.imwrite('facepic.jpg',frame)
+
+    # Hit 'q' on the keyboard to quit!
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
+# -*- coding: utf-8 -*-
+
+
+
+
+COS_ENDPOINT = "https://s3.jp-tok.cloud-object-storage.appdomain.cloud"
+#s3.us-south.cloud-object-storage.appdomain.cloud" # Current list avaiable at https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints
+COS_API_KEY_ID = "guft-62iXJ2do-8HAabO6u19JARQa5kMYvG2xtGFC4CU" # eg "W00YiRnLW4a3fTjMB-oiB-2ySfTrFBIQQWanc--P3byk"
+COS_AUTH_ENDPOINT = "https://iam.cloud.ibm.com/identity/token"
+COS_RESOURCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/79bdb6666e7345609b949a5a9b23cfa1:27a4b271-6711-45f5-b48e-64129234732f::"
+
+
+# Create resource
+cos = ibm_boto3.resource("s3",
+    ibm_api_key_id=COS_API_KEY_ID,
+    ibm_service_instance_id=COS_RESOURCE_CRN,
+    ibm_auth_endpoint=COS_AUTH_ENDPOINT,
+    config=Config(signature_version="oauth"),
+    endpoint_url=COS_ENDPOINT
+)
+        
+        
+def multi_part_upload(bucket_name, item_name, file_path):
+    try:
+        print("Starting file transfer for {0} to bucket: {1}\n".format(item_name, bucket_name))
+        # set 5 MB chunks
+        part_size = 1024 * 1024 * 10
+
+        # set threadhold to 15 MB
+        file_threshold = 1024 * 1024 * 25
+
+        # set the transfer threshold and chunk size
+        transfer_config = ibm_boto3.s3.transfer.TransferConfig(
+            multipart_threshold=file_threshold,
+            multipart_chunksize=part_size
+        )
+
+        # the upload_fileobj method will automatically execute a multi-part upload
+        # in 5 MB chunks for all files over 15 MB
+        with open(file_path, "rb") as file_data:
+            cos.Object(bucket_name, item_name).upload_fileobj(
+                Fileobj=file_data,
+                Config=transfer_config
+            )
+
+        print("Transfer for {0} Complete!\n".format(item_name))
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to complete multi-part upload: {0}".format(e))
+        
+
+            
+multi_part_upload("ramachandrasai","facepic.jpg","F:/SmartInternz-IoT-Externship-2021-master/Smart Home security codes/facepic.jpg")      
+            
